b37ed4560b2343f4bbc2b2f1e10b362512677b4e
Reesab Pathak <rpathak@stanford.edu>
2016-07-10 15:16:21 -0700

    defined base command clas iin base.py

---
+++ pdf-miner/commands/base.py
@@ -0,0 +1,15 @@
+# pdf-miner/commands/base.py
+""" This file describes the base command """
+
+# TODO: switch from base class to ABCMeta...
+
+class Base(object):
+    """ A base command object """
+    
+    def __init__(self, options, *args, **kwargs):
+        self.options = options
+        self.args = args
+        self.kwargs = kwargs
+    
+    def run(self):
+        raise NotImplementedError("You must implement the run() method")
